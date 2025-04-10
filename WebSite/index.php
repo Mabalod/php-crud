af992ae5cc7a9181782ce269ab1603e8a8433e86
Mabalod <kurtcollinmabalod@gmail.com>
2025-04-10 03:17:21 -0700

    Add files via upload

---
+++ WebSite/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+
+ $name = "Kurt Collin Clint B. Mabalod";
+ $age = "21 yrs old";
+ $place = "Dulapo Oroquieta City";
+ $hobby = "basketball";
+
+ echo " Hi my name is {$name} <br>I'm {$age} <br> I live in {$place} and I love playing {$hobby} ";
+?>
+
+    
+
+</body>
+</html>
